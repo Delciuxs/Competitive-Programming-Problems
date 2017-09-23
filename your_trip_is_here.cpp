ef459a31835327510a00d3f5a5f32dfe636958ba
Juan José Perusquia <32141279+Delciuxs@users.noreply.github.com>
2017-09-22 20:37:00 -0500

    Add files via upload

---
+++ your_trip_is_here.cpp
@@ -0,0 +1,36 @@
+//LINK to the Problem:
+//https://omegaup.com/arena/problem/ride#problems
+//Problem type: Modular Arithmetic
+
+/*
+For the solution of this problem I use property:
+(x mod n) * (y mod n) = (x*y mod n)
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	string comet, group;
+	cin >> comet >> group;
+	int result  = 1, result2 = 1;
+
+	for(int i = 0; i < comet.size(); i++)
+	{
+		result = result * (comet[i] - 'A' + 1 )%47;
+		result = result%47;
+	}
+
+	for(int i = 0; i < group.size(); i++)
+	{
+		result2 = result2 * (group[i] - 'A' + 1)%47;
+		result2 = result2%47;
+	}
+
+	if(result == result2)
+		cout << "GO" << endl;
+	else
+		cout << "STAY" << endl;
+	return 0;
+}
