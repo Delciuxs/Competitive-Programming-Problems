830ca046589958f153dbf1d6f0c7a1f3310b91ea
Juan Perusquia <foxmuller21@hotmail.com>
2019-01-08 16:29:31 -0600

    Create integer_inquirypy.py

---
+++ BigInteger/integer_inquirypy.py
@@ -0,0 +1,18 @@
+#LINK to the Problem:
+#https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=365
+#Problem type: Big Integer
+
+from sys import stdin, stdout
+
+def main():
+	number = stdin.readline()
+	number = int(number)
+	total_sum = 0
+	while(number != 0):
+		total_sum += number
+		number = stdin.readline()
+		number = int(number)
+
+	stdout.write(str(total_sum) + "\n")
+
+main()	
