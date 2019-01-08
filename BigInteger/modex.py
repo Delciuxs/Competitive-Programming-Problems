98e078875be0071185379923d2c96d0bd309a3ca
Juan Perusquia <foxmuller21@hotmail.com>
2019-01-08 16:29:38 -0600

    Create modex.py

---
+++ BigInteger/modex.py
@@ -0,0 +1,11 @@
+#LINK to the Problem:
+#https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3671
+#Problem type: Big Integer
+
+def main():
+	t = int(input())
+	for i in range(t):
+		x, y , z = map(int, input().split())
+		print(pow(x,y,z))
+
+main()
