f80f7cbf9da1a87ee9417dad90f89e97cb1b84a6
Juan Perusquia <foxmuller21@hotmail.com>
2019-01-08 16:29:46 -0600

    Create very_easy.py

---
+++ BigInteger/very_easy.py
@@ -0,0 +1,21 @@
+#LINK to the Problem:
+#https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1464
+#Problem type: Big Integer
+
+#We need funtion pow for a good aproximation
+
+import math
+import sys
+
+
+def main():
+	for query in sys.stdin:
+		n, a = query.split()
+		n = int(n)
+		a = int(a)
+		total_sum = 0
+
+		for i in range(1, n + 1):
+			total_sum += int(i*pow(a,i))
+		print(str(total_sum))
+main()
