dce414048b143c7f9384d6b01d6772a7cf5b02b0
Juan Perusquia <foxmuller21@hotmail.com>
2019-01-08 16:29:35 -0600

    Create Krakovia.py

---
+++ BigInteger/Krakovia.py
@@ -0,0 +1,27 @@
+#LINK to the Problem:
+#https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1866
+#Problem type: Big Integer
+
+#We need funtion Fractions for a good aproximation for a division
+from fractions import Fraction 
+
+def main():
+	cont = 0
+	while 1 :
+		query = input()
+		items, friends = query.split()
+		items = int(items)
+		friends = int(friends)
+		prize_items = []
+		total = 0
+		cont = cont + 1
+		if(items == 0 and friends == 0):
+			break
+		for i in range(0,items):
+			prize_items.append(input())
+		for i in range(0,items):
+			total += int(prize_items[i])
+		individual = Fraction(total,friends)
+		print("Bill #" +str(cont) + " costs " + str(total) + ": each friend should pay " + str(int(individual)))
+		print("")
+main()
