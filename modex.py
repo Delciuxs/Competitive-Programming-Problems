629a3bbb8a4eef473a84a8d7ae93f1097a189c61
Juan Perusquia <foxmuller21@hotmail.com>
2019-01-08 16:27:47 -0600

    Update modex.py

--- modex.py
+++ modex.py
@@ -1,6 +1,6 @@
 #LINK to the Problem:
 #https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3671
-#Problem type: Big Interger
+#Problem type: Big Integer
 
 def main():
 	t = int(input())
