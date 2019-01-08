af9a8203a637b205cc8ff0fed0acafb2569c8dfa
Juan Perusquia <foxmuller21@hotmail.com>
2019-01-08 16:28:19 -0600

    Update integer_inquirypy.py

--- integer_inquirypy.py
+++ integer_inquirypy.py
@@ -1,6 +1,6 @@
 #LINK to the Problem:
 #https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=365
-#Problem type: Big Interger
+#Problem type: Big Integer
 
 from sys import stdin, stdout
 
