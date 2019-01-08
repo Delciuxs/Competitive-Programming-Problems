fe263e3cee8d465df3ec6c15a3f84052ff1f236e
Juan Perusquia <foxmuller21@hotmail.com>
2019-01-08 16:28:12 -0600

    Update exponentiation.py

--- exponentiation.py
+++ exponentiation.py
@@ -1,6 +1,6 @@
 #LINK to the Problem:
 #https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=689
-#Problem type: Big Interger
+#Problem type: Big Integer
 
 import sys
 
